61eb41198a7f37e6ed2333c01c32e1d08501b8a7
Tommaso <tommaso.meledina@it.ibm.com>
2024-08-26 15:43:38 +0200

    chore: add minor details to the frontend skeleton

--- dmm-frontend/src/main/angular/src/app/admin/admin.module.ts
+++ dmm-frontend/src/main/angular/src/app/admin/admin.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NodeDetailsComponent } from './node-details/node-details.component';
 import { NodesTreeComponent } from './nodes-tree/nodes-tree.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 
 @NgModule({
   declarations: [
     NodeDetailsComponent,
-    NodesTreeComponent
+    NodesTreeComponent,
+    AdminComponent
   ],
   imports: [
     CommonModule
--- dmm-frontend/src/main/angular/src/app/app-routing.module.ts
+++ dmm-frontend/src/main/angular/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing/landing.component';
+import { GameViewComponent } from './play/game-view/game-view.component';
+import { AdminComponent } from './admin/admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'landing',
+    component : LandingComponent
+  },
+  {
+    path : 'play',
+    component : GameViewComponent
+  },
+  {
+    path : 'admin',
+    component : AdminComponent
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
