b791b45e7aef728a48565a54413209c5a0690d02
Tommaso <tommaso.meledina@it.ibm.com>
2024-10-01 18:58:03 +0200

    chore: change the Rel of LanguageAwareText item resources
    this is forced by the limitations of the logic I wrote on the FE side: if names do nt match, we get into an infinite recursion while exploring entity links

--- dmm-backend/dmm-be-core/src/main/java/a/posto/interactive/dmm/backend/core/repository/LanguageAwareTextRepository.java
+++ dmm-backend/dmm-be-core/src/main/java/a/posto/interactive/dmm/backend/core/repository/LanguageAwareTextRepository.java
@@ -4,6 +4,6 @@ import a.posto.interactive.dmm.backend.model.physical.entity.LanguageAwareText;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "texts", collectionResourceRel = "texts", itemResourceRel = "text")
+@RepositoryRestResource(path = "texts", collectionResourceRel = "texts", itemResourceRel = "texts")
 public interface LanguageAwareTextRepository extends JpaRepository<LanguageAwareText, Long> {
 }
