5a0ef55727531d5133e4a5d11acd0a0e1d526db2
Tommaso <tommaso.meledina@it.ibm.com>
2024-10-03 09:45:00 +0200

    refactor: fully separate product data from the core app
    also start the separation between BE and FE

--- dmm-backend/dmm-be-core/src/main/java/a/posto/interactive/dmm/backend/core/Application.java
+++ dmm-backend/dmm-be-core/src/main/java/a/posto/interactive/dmm/backend/core/Application.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "a.posto.interactive.dmm.backend")
 @EntityScan(basePackages = {"a.posto.interactive.dmm.backend.model"})
+@PropertySource(value = "classpath:product.properties")
 public class Application {
 
     public static void main(String[] args) {
